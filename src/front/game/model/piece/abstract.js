4e4882edc1f90a84b783a73795fbd3a241209a6e
Ivan Rubinson <soryy708@gmail.com>
2019-08-24 23:11:08 +0300

    Implement checking path vacancy

--- src/front/game/model/piece/abstract.js
+++ src/front/game/model/piece/abstract.js
@@ -1,3 +1,5 @@
+import modelConst from '../const';
+
 export default class AbstractPiece {
     constructor(boardState, x, y) {
         this.color = boardState[y][x].getColor();
@@ -35,6 +37,43 @@ export default class AbstractPiece {
             }
         }
 
+        return true;
+    }
+
+    pathIsVacant(to) {
+        /**
+         * Like `Math.ceil` but if the parameter is negative, floor.
+         * @param {Number} float 
+         */
+        function absoluteCeil(float) {
+            if (float >= 0) {
+                return Math.ceil(float);
+            } else {
+                return Math.floor(float);
+            }
+        }
+
+        const movementVector = {x: to.x - this.x, y: to.y - this.y};
+        const movementDistance = Math.sqrt(Math.pow(movementVector.x, 2) + Math.pow(movementVector.y, 2)); // `movementVector` magnitude
+        const movementDirection = {x: movementVector.x / movementDistance, y: movementVector.y / movementDistance}; // normalized `movementVector`
+        const discreteMovementDirection = {x: absoluteCeil(movementDirection.x), y: absoluteCeil(movementDirection.y)};
+
+        let x = this.x + discreteMovementDirection.x;
+        let y = this.y + discreteMovementDirection.y;
+        while (x != to.x || y != to.y) {
+            if (x < 0 || y < 0 || x >= modelConst.chess_board_width || y >= modelConst.chess_board_height) {
+                console.warn('Piece movement validation check for path vacancy coordinates out of bounds');
+                break;
+            }
+
+            const boardCell = this.boardState[y][x];
+            if (boardCell.isPiece()) {
+                return false;
+            }
+
+            x += discreteMovementDirection.x;
+            y += discreteMovementDirection.y;
+        }
         return true;
     }
 }
