dad4f298898c12f9345668d286992e2c1240456e
Ivan Rubinson <soryy708@gmail.com>
2019-08-24 23:18:07 +0300

    Add castling description

--- src/front/game/model/piece/king.js
+++ src/front/game/model/piece/king.js
@@ -20,6 +20,13 @@ export default class King extends AbstractPiece {
         // TODO: Can not move if results in check/mate
 
         // TODO: Castling
+        // This moves the king two cells left or right, and the rook (on the left or right respectively) to the cell the king crossed
+        // Predicates:
+        // 1. King was never moved
+        // 2. Rook involved was never moved
+        // 3. The cells between the king & rook are unoccupied
+        // 4. The king is not in check
+        // 5. The king does not cross over or end on a cell attacked by an enemy piece
 
         return true;
     }
